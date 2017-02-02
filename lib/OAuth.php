7422e6292023899f52cb35e488c10a7cbb7309bb
Graham-72 <g.oliver@btinternet.com>
2017-02-02 07:18:34 +0000

    Cannot redeclare class

--- lib/OAuth.php
+++ lib/OAuth.php
@@ -17,18 +17,20 @@ if (!class_exists('OAuthException')) {
   }
 }
 
-class OAuthConsumer {
-  public $key;
-  public $secret;
+if (!class_exists('OAuthConsumer')) {
+  class OAuthConsumer {
+    public $key;
+    public $secret;
 
-  function __construct($key, $secret, $callback_url=NULL) {
-    $this->key = $key;
-    $this->secret = $secret;
-    $this->callback_url = $callback_url;
-  }
+    function __construct($key, $secret, $callback_url = NULL) {
+      $this->key = $key;
+      $this->secret = $secret;
+      $this->callback_url = $callback_url;
+    }
 
-  function __toString() {
-    return "OAuthConsumer[key=$this->key,secret=$this->secret]";
+    function __toString() {
+      return "OAuthConsumer[key=$this->key,secret=$this->secret]";
+    }
   }
 }
 
